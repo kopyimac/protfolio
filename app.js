b030541d793e36c11bd529289ac704467614b50b
Pyi Soeaung <kopyimac@gmail.com>
2023-12-19 01:04:48 +0900

    portfolio

---
+++ app.js
@@ -0,0 +1,11 @@
+// Add your custom JavaScript code here
+
+// Example: Form submission handling
+document.getElementById('contactForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    // Add your form submission logic here
+    // For example, you can use Fetch API to send form data to a server
+    // and display a success message to the user
+    alert('Form submitted!');
+});
